046c40ef99a6604049b6f72d80a96772c5b984b9
Freda Toal <Freda.toal.work@gmail.com>
2023-04-07 16:42:31 +0000

    Add function for intro screen and game instructions

--- val.py
+++ val.py
@@ -4,3 +4,22 @@ import random
 # Function to clear the terminal
 def clear():
     os.system(('cls' if os.name == 'nt' else 'clear'))
+
+# Function for the intro screen and instructions
+def intro_screen(): 
+    print("Welcome to 'Voyage for Valhalla'\n")
+    print("""
+              |    |    |                Voyage for Valhalla is a word game.
+             )_)  )_)  )_)               If a letter you guess is in the chosen
+            )___))___))___)\             random word, it will show.
+           )____)____)_____)\\            If a guessed letter is not in the
+         _____|____|____|____\\\__        chosen word, you will lose a life.
+---------\                   /---------  You have 5 lives to guess before the
+  ^^^^^ ^^^^^^^^^^^^^^^^^^^^^            boat sinks and you lose the game.
+    ^^^^      ^^^^     ^^^    ^^    
+         ^^^^      ^^^                   Good luck!
+    """)
+    print("---------------------------------------")
+    name = input("Enter your name here to begin play: ")
+    clear()
+    return name  
