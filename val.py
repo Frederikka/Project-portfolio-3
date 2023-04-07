66560945e19ceab2267d177f047a4e81aba3e233
Freda Toal <Freda.toal.work@gmail.com>
2023-04-07 16:53:13 +0000

    Add main function with while loop for answer options

--- val.py
+++ val.py
@@ -79,4 +79,81 @@ def print_boat_stages(lives):
          ^^^^      ^^^
             """)         
 
-           
+# Function to return word from words list
+def get_words():
+    return [
+        'delightful',
+        'sound',
+        'shoes',
+        'connect',
+        'malicious',
+        'explode',
+        'spooky',
+        'pies',
+        'fill',
+        'fowl',
+        'vessel',
+        'awake',
+        'billowy',
+        'green',
+        'sugar',
+        'remove',
+        'marvelous',
+        'bag',
+        'jealous',
+        'prevent'
+    ]   
+
+# Function for main functions with while loop for answer options
+def main():
+    name = intro_screen()
+    while True:
+        guessed_letters = []
+        lives = 5
+        current_word = []
+        guessed = False
+        answer = random.choice(get_words())
+        current_word = ['_' for x in range(len(answer))]
+
+        while not guessed and lives > 0:
+            print_boat_stages(lives)
+            print(f"Lives: {lives}")
+            print(f'Current word: {" ".join(current_word)}\n')
+            user_guess = input(f'Please guess a letter, {name}: ')
+            clear()
+            if len(user_guess) == 1 and user_guess.isalpha():
+                if user_guess in guessed_letters:
+                    print(f'You already guessed the letter "{user_guess.upper()}". Please try again! ')
+                elif user_guess not in answer:
+                    print(f'"{user_guess.upper()}" is not in the word! Please try again!')
+                    lives -= 1
+                    guessed_letters.append(user_guess)
+                else:
+                    print(f'"{user_guess.upper()}" is in the word, well done!')
+                    guessed_letters.append(user_guess)
+                    for index in range(0, len(answer)):
+                        if answer[index] == user_guess:
+                            current_word[index] = user_guess
+                    if answer == ''.join(current_word):
+                        guessed = True
+            elif len(user_guess) == len(answer) and user_guess.isalpha():
+                if user_guess != answer:
+                    print(f'"{user_guess.upper()}" is not in the word')
+                    lives -= 1
+                else:
+                    guessed = True
+            else:
+                print('Not a valid response, please try again!')
+        if lives == 0:
+            print("""
+            UH OH...
+  ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+    ^^^^      ^^^^     ^^^    ^^
+         ^^^^      ^^^
+            """) 
+            print(f'You died, {name}. Oh dear.. The word was "{answer}".')
+        else:
+            print(f'You won, {name}! Congratulations, you live to fight another day. The word was "{answer}".')
+        input('Press any key to try again! ')
+        clear()
+         
